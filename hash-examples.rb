65a5d7f99ca4d094f50435aa85a451c69419411c
Sam Smith <samlsmith424@gmail.com>
2021-10-20 19:19:48 -0700

    Add animal hash

--- hash-examples.rb
+++ hash-examples.rb
@@ -1,5 +1,11 @@
 car_hash = {
-  "brand" => "Audi"
-  "model" => "R8"
+  "brand" => "Audi",
+  "model" => "R8",
   "year" => 2020
+}
+
+animal_hash = {
+  type: "lion",
+  family: ['baby lion', 'mother lion'],
+  how_old: 13
 }
